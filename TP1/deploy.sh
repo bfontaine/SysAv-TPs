2045f8c3dbb38cfdebc815f379f9e9fcf0da7b11
Baptiste Fontaine <batifon@yahoo.fr>
2015-01-29 17:39:56 +0100

    deploy fixed to not include garbage in the tgz

--- TP1/deploy.sh
+++ TP1/deploy.sh
@@ -11,4 +11,5 @@ pandoc --template tpl.tex --latex-engine=xelatex rapport.md -o rapport.pdf
 popd
 
 cp -r rendu $TARGET
-tar czvf ${TARGET}.tgz --exclude '*.md' --exclude '*.tex' $TARGET
+tar czvf ${TARGET}.tgz --exclude '*.md' --exclude '*.tex' \
+  --exclude '*.dSYM' --exclude '.gitignore' $TARGET
